4b1caa4d032b82aab450be65fa326dd9744aaba1
tungnk_2711 <tungdemo@gmail.com>
2018-04-14 22:05:07 +0700

    update project

--- CheckLeapYear/CheckLeapYear/Program.cs
+++ CheckLeapYear/CheckLeapYear/Program.cs
@@ -10,9 +10,12 @@ namespace CheckLeapYear
     {
         static void Main(string[] args)
         {
+            //Khai báo biến sử dụng trong chương trình
+            int year;
+
             //Nhập vào năm cần kiểm tra 
             Console.Write("Please enter a year: ");
-            int year = Int32.Parse(Console.ReadLine());
+            year = Int32.Parse(Console.ReadLine());
 
             //Kiểm tra và hiển thị kết quả
             if (year % 4 == 0)
@@ -22,13 +25,13 @@ namespace CheckLeapYear
                     if (year % 400 == 0)
                         Console.WriteLine("{0} is a leap year.", year);
                     else
-                        Console.WriteLine("{0} is not a leap year.", year);
+                        Console.WriteLine("{0} is NOT a leap year.", year);
                 }
                 else
                     Console.WriteLine("{0} is a leap year.", year);
             }
             else
-                Console.WriteLine("{0} is not a leap year.", year);
+                Console.WriteLine("{0} is NOT a leap year.", year);
 
             Console.ReadLine();
         }
